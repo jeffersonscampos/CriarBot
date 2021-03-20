edf66a91515a521c9c4f2d451ada495ee92bcab5
Jefferson de Souza Campos <jeffsouzanet@hotmail.com>
2021-03-20 18:48:07 -0300

    Teste com MongoClient

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ const path = require('path')
   
   const express = require('express');
   const app = express();
-  //const MongoClient = require('mongodb').MongoClient;
-  var MongoClient = require('mongoose');
+  const MongoClient = require('mongodb').MongoClient;
+  //var MongoClient = require('mongoose');
   const assert = require('assert');
   const bodyParser = require('body-parser');
   
@@ -32,11 +32,13 @@ const path = require('path')
   app.use('/js', express.static(__dirname + '/js/jquery-3.5.1'));
   app.use('/js', express.static(__dirname + '/js/bootstrap-4.5.3-dist'));
   
+  
   MongoClient.connect(url, {useUnifiedTopology: true, useNewUrlParser: true}, function(err, client){
       assert.equal(null, err);
       console.log('banco de dados conectado com sucesso!');
   
       db = client.db(dbName);
+  
   });
   
   //app.listen(3000);
